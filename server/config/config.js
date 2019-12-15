f879b028aa7180b2f3a80ce97e20ad9b29dc47c5
eduardoislas <eislas22@gmail.com>
2019-12-15 14:01:40 -0700

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,17 @@
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+
+// Base de Datos
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe-user:xIQWA8rXwi0KuviP@cluster0-amt3e.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
